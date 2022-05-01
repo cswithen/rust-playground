379095e591d67fc1d370ae7a9034993c4c7d3b1c
cswithen <cody.swithenbank@gmail.com>
2022-04-30 19:57:07 -0700

    feat: variables

---
+++ 02-types-and-variables/variables/src/main.rs
@@ -0,0 +1,44 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+
+// standard library to be used within the code
+use std::mem;
+
+fn main() {
+    // number
+    let a: u8 = 123; // u = unsigned, 8 bits, 0 -- 255
+    println!("a = {}", a); // immutable (inside rust all variables are immutable)
+
+    // mutable due to the "mut" keyword
+    let mut b: i8 = 0; // i = signed, -128 -- 127
+    println!("b = {} before", b);
+
+    b = 42;
+    println!("b = {} after", b);
+
+    // variable type can be set using inference
+    let mut c = 123456789; // i32 = 32 bits = 4 bytes
+    println!("c = {}, takes up {} bytes", c, mem::size_of_val(&c));
+    c= -1;
+    println!("c = {} after", c);
+
+    // u8, u16, u32, u64, i8, i16, ...
+
+    // usize and isize
+    let z: isize = 123;
+    let size_of_z = mem::size_of_val(&z); //usize
+
+    println!("z = {}, takes up {} bytes, {}-bit OS", z, size_of_z, size_of_z*8);
+
+    // character
+    let d: char = 'x';
+    println!("{} is a char, size = {} bytes", d, mem::size_of_val(&d));
+
+    // f32 and f64 (floats) "IEEE754" signed!
+    let e: f64 = 2.5;
+    println!("{}, size = {} bytes", e, mem::size_of_val(&e));
+
+    // boolean
+    let g: bool = false; // true, false
+    println!("{}, size = {} bytes", g, mem::size_of_val(&g));
+}
