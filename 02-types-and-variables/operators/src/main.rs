c946b3ed530f62cc57fd30bf1102ad8b7d78590e
cswithen <cody.swithenbank@gmail.com>
2022-04-30 20:16:19 -0700

    feat: operators

---
+++ 02-types-and-variables/operators/src/main.rs
@@ -0,0 +1,45 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+
+// standard library to be used within the code
+// use std::mem;
+
+fn operators()
+{
+    // arithmetic
+
+    let mut a = 2+3*4; // +-*/
+    println!("a = {}", a);
+    a = a+1; // does not support -- or ++
+    a -= 2;// -= += *= /= %=
+
+    println!("remainder of {} / {} = {}", a, 3, (a%3));
+
+    let a_cubed = i32::pow(a, 3);
+    println!("a_cubed = {} ", a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+
+    println!("{} cubed = {}, {}^pi = {}", b, b_cubed, b, b_to_pi);
+
+    // bitwise
+    let c = 1 | 2; // | OR, & AND, ^ XOR, ! NOR
+    println!("1|2 = {}", c);
+
+    let two_to_10 = 1 << 10; // >>
+    println!("2^10 = {}", two_to_10);
+
+    // logical
+    let pi_less_4 = std::f64::consts::PI < 4.0; // true, > <= >= ==
+    println!("is pi less than 4, {}", pi_less_4);
+
+    let x = 5;
+    let x_is_5 = x == 5; // true
+    println!("is x equal to 5, {}", x_is_5);
+}
+
+fn main() {
+    operators()
+}
