0f79ce6a3da0119957a9c98513d9cb3294911687
Cody Swithenbank <cody.swithenbank@gmail.com>
2022-05-08 13:48:26 -0700

    feat: match statement and combination lock

--- 03-control-flow/control-flow/src/main.rs
+++ 03-control-flow/control-flow/src/main.rs
@@ -1,3 +1,7 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_must_use)]
+
 fn if_statement() {
     // variable declaration
     let temp = 35;
@@ -81,8 +85,70 @@ fn for_loop() {
     }
 }
 
+fn match_statement() {
+    let country_code = 7;
+
+    // rust is smart enough to yell at you for not covering all values, therefore the "_"
+    let country = match country_code {
+        44 => "UK",
+        46 => "Sweden",
+        7 => "Russia",
+        1..=1000 => "unknown",
+        _ => "invalid",
+    };
+
+    println!("the country with code {} is {}", country_code, country)
+}
+
+// use rand::Rng;
+use std::io::stdin;
+
+enum State {
+    Locked,
+    Failed,
+    Unlocked,
+}
+
+fn combination_lock() {
+    let code = String::from("1234");
+    let mut state = State::Locked;
+    let mut entry = String::new();
+
+    loop {
+        match state {
+            State::Locked => {
+                let mut input = String::new();
+                match stdin().read_line(&mut input) {
+                    Ok(_) => entry.push_str(&input.trim_end()),
+                    Err(_) => continue,
+                }
+
+                if entry == code {
+                    state = State::Unlocked;
+                    continue;
+                }
+
+                if !code.starts_with(&entry) {
+                    state = State::Failed
+                }
+            }
+            State::Failed => {
+                println!("FAILED");
+                entry.clear();
+                state = State::Locked
+            }
+            State::Unlocked => {
+                println!("UNLOCKED");
+                return;
+            }
+        }
+    }
+}
+
 fn main() {
     if_statement();
     while_and_loop();
     for_loop();
+    match_statement();
+    combination_lock();
 }
