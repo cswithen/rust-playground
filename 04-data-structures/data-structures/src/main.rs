fd72af44f106d574b4e76a91da611bab56307dc3
Cody Swithenbank <cody.swithenbank@gmail.com>
2022-05-08 15:10:13 -0700

    feat: structs and enums

---
+++ 04-data-structures/data-structures/src/main.rs
@@ -0,0 +1,31 @@
+mod enums;
+
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+struct Line {
+    start: Point,
+    end: Point,
+}
+
+fn structures() {
+    let p = Point { x: 3.0, y: 4.0 };
+
+    println!("point P is at ({}, {})", p.x, p.y);
+
+    let p2 = Point { x: 5.0, y: 10.0 };
+
+    let myline = Line { start: p, end: p2 };
+
+    println!(
+        "myLine starts at ({},{}), and ends at ({},{})",
+        myline.start.x, myline.start.y, myline.end.x, myline.end.y
+    )
+}
+
+fn main() {
+    structures();
+    enums::enums();
+}
